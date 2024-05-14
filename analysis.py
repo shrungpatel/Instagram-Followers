525c1243cc84c8c18e162001fd6a2560c2fe0991
shrungpatel <106874421+shrungpatel@users.noreply.github.com>
2024-05-14 09:25:37 -0400

    Useed set comprehensions so it's only 4 lines now!

--- analysis.py
+++ analysis.py
@@ -1,17 +1,4 @@
 import json
-
-following = open('following.json')
-data = json.load(following)
-following_set = set()
-for person in data['relationships_following']:
-    following_set.add(person['string_list_data'][0]['value'])
-following.close()
-
-followers = open('followers_1.json')
-data = json.load(followers)
-followers_set = set()
-for person in data:
-    followers_set.add(person['string_list_data'][0]['value'])
-followers.close()
-
+following_set = {person['string_list_data'][0]['value'] for person in json.load(open('following.json'))['relationships_following'] }
+followers_set = {person['string_list_data'][0]['value'] for person in json.load(open('followers_1.json')) }
 print(following_set.difference(followers_set))
