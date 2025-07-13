d98ef0eb73ec563e19200f6297b971fa916e31d0
shrungpatel <shrungpatel@yahoo.in>
2025-07-13 15:31:35 -0400

    python logic works with the new format

--- analysis.py
+++ analysis.py
@@ -5,10 +5,11 @@ def get_list_of_people(file):
     """
     start_line = 5 if file.endswith('followers.txt') else 7
     with open(file, 'r') as f:
-        lines = f.read().splitlines()[start_line:]  # Skip the first 5 lines
+        lines = f.read().splitlines()[start_line:]  # Skip the first few lines with the header
+        # the file has "username url \n date \n" format
         return [lines[i].split()[0] for i in range(len(lines)) if i % 2 == 0 and lines[i] != '']
 
 followers = get_list_of_people('followers.txt')
 following = get_list_of_people('following.txt')
-following_but_not_following = set(following) - set(followers)
-print(following_but_not_following, len(following_but_not_following))
+following_but_not_following_back = set(following) - set(followers)
+print(following_but_not_following_back, len(following_but_not_following_back))
